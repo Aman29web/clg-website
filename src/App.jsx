96c9f6279684ac8760212743862b74079b5cabd9
Aman29web <amansinghvns29@gmail.com>
2024-09-21 12:03:15 +0530

    capmus dection competed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './copmonenets/Nav-bar/Navbar'
 import Hero from './copmonenets/Hero/Hero'
 import Programs from './copmonenets/Programs/Programs'
 import Title from './copmonenets/Title/Title'
+import About from './copmonenets/About/About'
+import Campus from './copmonenets/Campus/Campus'
 
 const App = () => {
   return (
@@ -12,6 +14,10 @@ const App = () => {
       <div className="container">
         <Title subtitle='Our PROGRAM' title='What We Offer'></Title>
       <Programs></Programs>
+      <About></About>
+      <Title subtitle='Gallery' title='Campus Photos'></Title>
+      <Campus></Campus>
+
       </div>
 
 
--- src/copmonenets/About/About.jsx
+++ src/copmonenets/About/About.jsx
@@ -14,6 +14,9 @@ const About = () => {
         <div className="about-right">
             <h3>About UNIVERSITY</h3>
             <h2>Nurturing Tomorrow's Leaders Today</h2>
+            <p>Embark on a transformative educational journey with our university's comprehesive educatioon program. Our cutting-edge curriculam is dedigned to empower studetns with the knowledge, skills and experience needed to excel inthe dynamic field of education.</p>
+            <p>With a ficus on innovation, hands-on learning and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classroom, schools and commonities,</p>
+            <p>Wheather you aspire to become a teacher, administrator, conseltor or educaional leader, our diverse range of program offers the perfect pathway to achive your goal and unlock your full potential in shaping the future if education.</p>
         </div>
       
     </div>
