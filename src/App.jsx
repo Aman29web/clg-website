77d8ad9fd1d0b2f18a69761a1e97b6cbee5b40cb
Aman29web <amansinghvns29@gmail.com>
2024-09-21 10:26:22 +0530

    Hero Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './copmonenets/Nav-bar/Navbar'
+import Hero from './copmonenets/Hero/Hero'
+import Programs from './copmonenets/Programs/Programs'
+import Title from './copmonenets/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Hero></Hero>
+      <div className="container">
+        <Title subtitle='Our PROGRAM' title='What We Offer'></Title>
+      <Programs></Programs>
+      </div>
+
+
     </div>
   )
 }
