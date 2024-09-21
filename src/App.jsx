416ff0617f0a062b4004ec154b867e65fe2bbb5e
Aman29web <amansinghvns29@gmail.com>
2024-09-21 17:18:46 +0530

    Unresposive site completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Programs from './copmonenets/Programs/Programs'
 import Title from './copmonenets/Title/Title'
 import About from './copmonenets/About/About'
 import Campus from './copmonenets/Campus/Campus'
+import Testimonials from './copmonenets/Testimonials/Testimonials'
+import Contact from './copmonenets/Contact/Contact'
+import Footer from './copmonenets/Footer/Footer'
 
 const App = () => {
   return (
@@ -17,6 +20,13 @@ const App = () => {
       <About></About>
       <Title subtitle='Gallery' title='Campus Photos'></Title>
       <Campus></Campus>
+      <Title subtitle='TESTIMONIALS' title='What Students Says'></Title>
+      <Testimonials></Testimonials>
+      <Title subtitle='Contact Us' title='Get in Touch'></Title>
+      <Contact></Contact>
+      <Footer></Footer>
+
+
 
       </div>
 
